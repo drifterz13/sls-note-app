e0277d986484923e6ea3d99b125da4d697f4fc1c
drifterz13 <mixmyxxe@gmail.com>
2021-05-03 15:21:39 +0700

    re-trigger after update IAM

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@
 module.exports.hello = async (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: `Hello world! 🍺🍺 ${process.env.MONGODB_USERNAME}`,
+    body: `Hello world! 🍺🍺🍺 ${process.env.MONGODB_USERNAME}`,
   };
 
   callback(null, response);
